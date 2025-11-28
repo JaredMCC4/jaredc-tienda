1cf1d112528ea1635a8fed5836dabf54e6d04a09
Jared <jchacon00466@ufide.ac.cr>
2025-11-27 20:32:58 -0600

    Semana 12
    carrito de compras

---
+++ src/main/java/org/jared/jaredctienda/repository/VentaRepository.java
@@ -0,0 +1,7 @@
+package org.jared.jaredctienda.repository;
+
+import org.jared.jaredctienda.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
+}
---
+++ src/main/java/org/jared/jaredctienda/services/FacturaService.java
@@ -0,0 +1,31 @@
+package org.jared.jaredctienda.services;
+
+import org.jared.jaredctienda.domain.Factura;
+import org.jared.jaredctienda.repository.FacturaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class FacturaService {
+
+    private final FacturaRepository facturaRepository;
+
+    public FacturaService(FacturaRepository facturaRepository) {
+        this.facturaRepository = facturaRepository;
+    }
+
+
+    @Transactional(readOnly = true) // Solo lectura, mejor rendimiento
+    public Factura getFacturaConVentas(Integer idFactura) {
+
+        // 1. Llamar al método del repositorio que usa FETCH JOIN
+        // El .orElseThrow lanza una excepción si el Optional está vacío
+        return facturaRepository.findByIdFacturaConDetalle(idFactura)
+                .orElseThrow(() -> new NoSuchElementException("Factura con ID " + idFactura + " no encontrada."));
+
+        // El resultado es una Factura con todas las relaciones (usuario, ventas, producto)
+        // ya inicializadas, lista para ser usada en el Controller y la vista Thymeleaf.
+    }
+}
